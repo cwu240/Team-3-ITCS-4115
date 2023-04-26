decc123b26cb53fbc54b44d60acee31719c1f38e
PeytonRoden <peyton.h.roden@gmail.com>
2023-04-26 14:28:55 -0400

    got refund button working

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_tickets, name="view_tickets"),
+    path(r'tickets/(?P<pk>[0-9]+)/$', views.ticket_delete, name='ticket_delete')
 
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.apps import apps
 import json 
 import ast
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def view_tickets(request):
    Tickets = apps.get_model('flight', 'Tickets')
 
-   ticket_list = list(Tickets.objects.filter(customer_id=request.user))
+
+   ticket_list1 = list(Tickets.objects.filter(customer_id=request.user))
+   ticket_list = []
 
 
-   for i in range(0, len(ticket_list)):
-      ticket_list[i] = ast.literal_eval(str(ticket_list[i].json_ticket_data))
+   for i in range(0, len(ticket_list1)):
+      ticket_list.append(ast.literal_eval(str(ticket_list1[i].json_ticket_data)))
 
    tickets_list_short = []
    for i in range(0, len(ticket_list)):
@@ -23,7 +27,7 @@ def view_tickets(request):
       #print(ticket_list[i]['flightOffers'][0]['itineraries'][0]['segments'] )
       #print(ticket_list[i]['flightOffers'][0]['itineraries'][0]['segments'][0]['departure']['iataCode'] )
       #print(ticket_list[i]['flightOffers'][0]['itineraries'][0]['segments'][-1]['arrival']['iataCode'] )
-
+      dicti['ticket'] = ticket_list1[i]
       dicti['dept_iata'] = ticket_list[i]['flightOffers'][0]['itineraries'][0]['segments'][0]['departure']['iataCode']
       dept_time = ticket_list[i]['flightOffers'][0]['itineraries'][0]['segments'][0]['departure']['at'].replace('T',' ').split(' ')
       dicti['dept_date'] = dept_time[0]
@@ -43,3 +47,13 @@ def view_tickets(request):
 
 
    return render(request, "tickets/tickets.html", {'tickets':ticket_list ,'short_list':tickets_list_short})
+
+
+def ticket_delete(request, pk):
+   Tickets = apps.get_model('flight', 'Tickets')
+   ticket = get_object_or_404(Tickets, pk=pk)
+   print(ticket)
+   if request.method == 'POST':
+      ticket.delete()
+      return redirect('view_tickets')
+   return redirect('view_tickets')
